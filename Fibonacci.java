48f311614d3275c841d384a067a69c47faa03ce0
Santa-123 <nigamsantvana@gmail.com>
2022-11-13 13:25:07 +0530

    inital commit

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+class Fibonacci
+{
+	public static void main(String[] args)
+	{
+		//program to print the fibonacci series
+		int a=0,b=1,n=8,c=0;
+		System.out.println(a);
+		System.out.println(b);
+		for(int i=1;i<=n-2;i++)
+		{
+			c=a;
+			a=b;
+			b=a+c;
+			System.out.println(b);
+		}
+	}
+}
